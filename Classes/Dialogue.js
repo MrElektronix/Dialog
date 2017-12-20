fad5e9e1a5c9b29794aa05f6586a0ff6e0711919
MrElektronix <milovanpelt@gmail.com>
2017-12-20 23:56:25 +0100

    Added Questions

--- Classes/Dialogue.js
+++ Classes/Dialogue.js
@@ -18,37 +18,108 @@ class Dialogue{
 		this.currentIndex = 0;
 		this.player = {
 			qa: [ 	{
-						question: "Hello how are you?",
-						answer1: {a: "Very Good !", Next: 1},
-						answer2: {a: "Not so good :(" , Next: 2},
-						answer3: {a: "I'm Vegan", Next: 3},
+						question: "What kind of shop do you wanna visit",
+						answer1: {a: "Magic Shop", Next: 1},
+						answer2: {a: "Armour Shop" , Next: 2},
+						answer3: {a: "Potion Shop", Next: 3},
 					},
 				 
 				 	{
-						question: "Why are you good?",
-						answer1: {a: "I smell like fish"},
-						answer2: {a: "Because i am married to justice"},
-						answer3: {a: "I just finished listening to screaming frogs"},
+						question: "Welcome to the Magic Shop, what would you like to buy?",
+						answer1: {a: "A fireball spell", Next: 3},
+						answer2: {a: "A frost spell", Next: 4},
+						answer3: {a: "A earth spell", Next: 5},
 					},
 				 
 				 	{
-						question: "What is wrong?",
-						answer1: {a: "I smell like fish"},
-						answer2: {a: "I lost my puppy"},
-						answer3: {a: "I saw myself in the mirror"},
+						question: "Welcome to the armour Shop, what would you like to buy?",
+						answer1: {a: " A pair of boots", Next: 5},
+						answer2: {a: " A helmet" , Next: 6},
+						answer3: {a: "A chest plate", Next: 7},
 					},
 				 
 				 	{
-						question: "Ehh well ok then",
-						answer1: {a: "How dare you to say that to me!"},
-						answer2: {a: "Remember to eat your vegetables"},
-						answer3: {a: "Cthulu will rise again !"},
+						question: "Welcome to the Potion Shop, what would you like to buy?",
+						answer1: {a: "A healing potion", Next: 7},
+						answer2: {a: "A damage potion", Next: 8},
+						answer3: {a: "A poision potion", Next: 9},
 					},
-					
-				
+				 
+				 	{
+						//fireball spell
+						question: "Thank you for purchasing a fireball spell",
+						answer1: {a: ""},
+						answer2: {a: ""},
+						answer3: {a: ""},
+					},
+				 
+				 	{
+						//frost spell
+						question: "Thank you for purchasing a frost spell",
+						answer1: {a: ""},
+						answer2: {a: ""},
+						answer3: {a: ""},
+					},
+				 	
+				 	{
+						//earth spell
+						question: "Thank you for purchasing a earth spell",
+						answer1: {a: ""},
+						answer2: {a: ""},
+						answer3: {a: ""},
+					},
+				 
+				 	{
+						//boots
+						question: "Thank you for purchasing boots",
+						answer1: {a: ""},
+						answer2: {a: ""},
+						answer3: {a: ""},
+					},
+				 
+				 	{
+						//helmet
+						question: "Thank you for purchasing a helmet",
+						answer1: {a: ""},
+						answer2: {a: ""},
+						answer3: {a: ""},
+					},
+				 	
+				 	{
+						//chestplate
+						question: "Thank you for purchasing a chestplate",
+						answer1: {a: ""},
+						answer2: {a: ""},
+						answer3: {a: ""},
+					},
+				 
+				 
+				 	 {
+						//health potion
+						question: "Thank you for purchasing a health potion",
+						answer1: {a: ""},
+						answer2: {a: ""},
+						answer3: {a: ""},
+					},
+				 
+				 	{
+						//damage potion
+						question: "Thank you for purchasing a damage potion",
+						answer1: {a: ""},
+						answer2: {a: ""},
+						answer3: {a: ""},
+					},
+				 	
+				 	{
+						//poision potion
+						question: "Thank you for purchasing a poision potion",
+						answer1: {a: ""},
+						answer2: {a: ""},
+						answer3: {a: ""},
+					},
+	
 				],
 		}
-		
 		this.refreshQA();
 	}
 	
@@ -60,16 +131,15 @@ class Dialogue{
 	}
 	
 	setNewQA(value) {
-		
 		if(value == "answer1") {
 			this.nextIndex = this.currentIndex += this.player.qa[this.currentIndex].answer1.Next;
-			
 			
 			this.question = this.player.qa[this.nextIndex].question;
 			this.answer1 = this.player.qa[this.nextIndex].answer1.a;
 			this.answer2 = this.player.qa[this.nextIndex].answer2.a;
 			this.answer3 = this.player.qa[this.nextIndex].answer3.a;
 		}
+		
 		if(value == "answer2") {
 			this.nextIndex = this.currentIndex += this.player.qa[this.currentIndex].answer2.Next;
 			
